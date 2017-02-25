de27e26a95dd0e7b241651b22bb05f1f96f0deea
pavic989 <pavicmilos989@gmail.com>
2017-02-25 19:13:11 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+ $(function(){
+	$('ul.nav li.dropdown').hover(function(){
+		$('.dropdown-menu', this).fadeIn();                   
+	}, function() {
+		$('.dropdown-menu', this).fadeOut('fast');
+	});
+});
+
+// ============= Hover dropdown menu setting =============
+
+
+if($('#second').height() < $('#first').height()){
+	$("#second").height($("#first").height());
+} else {
+	$("#first").height($("#second").height());
+}
+
+
+// ============= Sections equal height setting =============
+
+
+$(document).ready( function(){
+	$('.navbar-toggle').on('click', function(){
+		$(this).toggleClass('open');
+	});
+	})
+
+// ============= Animating burger button =============
+
+  $(function() {
+    $(".rslides").responsiveSlides();
+  });
+// ============= Slider setting =============
